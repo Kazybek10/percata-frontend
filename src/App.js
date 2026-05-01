da452d4532a67d0cfbe6d0aeecba73239c2466ef
Kazybek <kazybeknurlanbek@gmail.com>
2026-05-01 12:20:49 +0200

    feat: initial React setup with Rails API movies fetch

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/movies.json")
+      .then(res => res.json())
+      .then(data => setMovies(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Percata</h1>
+      {movies.map(movie => (
+        <p key={movie.id}>{movie.title}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
